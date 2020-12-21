aa84e194f76680dbe65ac2adb9ce9b1edc85ead2
CN194910 <panlijing1993@126.com>
2020-12-21 18:02:49 +0800

    修改logger.py文件

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
-import sys
+import time
 import logging
 from demo.utils.common_utils import CommonUtils
 
 class Logger(object):
-    log_path = CommonUtils.make_direction('log')
-    
+    def __init__(self, logger_name):
+        
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+        
+        rq = time.strftime('%Y%m%d%H%M%S', time.asctime(time.time()))
+        log_path = CommonUtils.make_direction('log')
+        
+        
+        fh = logging.FileHanadler("")
+        
 if __name__ == '__main__':
     logger = Logger()
