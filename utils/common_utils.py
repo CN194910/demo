6198cb1f25a5ab7d10dfb967b7f2566551b0c554
CN194910 <panlijing1993@126.com>
2020-12-25 16:52:39 +0800

    common_utils.py中增加delete_files

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,6 +1,10 @@
 #! /usr/bin/python3
 
 import os
+import time
+import datetime
+from demo.utils.logger import Logger
+
 
 
 class CommonUtils(object):
@@ -25,6 +29,35 @@ class CommonUtils(object):
             return filename
         elif filename:
             return os.path.join(path, filename)
+            
+    @staticmethod
+    def delete_files(path, offset):
+        logger = Logger('delete_files').get_logger()
+        #获取当前时间
+        current_time = datetime.datetime.now()
+        #设置时间偏移量
+        time_offset = datetime.timedelta(days=-offset)
+        #获取想要的日期
+        re_date = (current_time + time_offset)
+        #将时间转换成时间戳
+        re_date_unix = time.mktime(re_date.timetuple())
+        logger.info('删除{}目录下,{}天前的文件'.format(path, offset))
+        
+        #递归遍历给定的路径
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                #拼接路径和文件
+                filename = os.path.join(root, file)
+                #获取文件修改时间（时间戳）
+                file_mtime = os.path.getmtime(filename)
+                #把时间转换为人类可读时间
+                timeArray = time.localtime(file_time)
+                human_readable_time = time.strftime('%Y%m%d%H%M%S', timeArray)
+                
+                if file_mtime <= re_date_unix:
+                    #删除文件
+                    os.remove(filename)
+                    logger.info('删除文件: {},文件修改时间: {}'.format(filename, human_readable_time))
 
 
 if __name__ == '__main__':
